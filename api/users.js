a65bfd0ed1102a1d059e02b827ad37b8af2629fc
halbel01 <125689963+halbel01@users.noreply.github.com>
2024-12-06 16:46:11 +0000

    Update users.js

--- api/users.js
+++ api/users.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { TextEncoder } = require('util');
 const User = require('../schemas/schemaUser');
 
 router.post('/register', async (request, response) => {
