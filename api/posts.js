8ce05faf154267fed14b4fae00392b76d49c00b8
halbel01 <125689963+halbel01@users.noreply.github.com>
2024-12-08 17:35:33 +0000

    Update posts.js

--- api/posts.js
+++ api/posts.js
@@ -20,9 +20,9 @@ module.exports = (database) => {
       };
 // Utilizing the data provided to create a new post.
             const docRef = await posts.add(newPost);
-      res.status(201).json({ id: docRef.id, ...newPost });
+      response.status(201).json({ id: docRef.id, ...newPost });
     } catch (err) {
-      res.status(400).send(err.message);
+      response.status(400).send(err.message);
     }
   });
 
@@ -30,40 +30,43 @@ module.exports = (database) => {
 
 router.post('/:id/like', access, async (request, response) => {
     try {
-      const postRef = postsCollection.doc(req.params.id);
+      const postRef = posts.doc(request.params.id);
       const post = await postRef.get();
 
       if (!post.exists) {
-        return res.status(404).send('Post not found');
+        return response.status(404).send('Post not found');
       }
             const postData = post.data();
-      postData.likes.push(req.body.userId);
+      postData.likes.push(request.body.userId);
       await postRef.update({ likes: postData.likes });
 
-      res.send('Liked');
+      response.send('Liked');
     } catch (err) {
-      res.status(400).send(err.message);
+      response.status(400).send(err.message);
     }
   });
 
 // Commenting on post
-router.post('/:id/comment', access, async (req, res) => {
-  const comment = { body: req.body.body, date: new Date(), user: req.user._id };
+router.post('/:id/comment', access, async (request, response) => {
+  const comment = { body: request.body.body, date: new Date(), user: request.user._id };
   // Constructing a comment object including the user's ID, the comment body, and the current date.
+  
   try {
-    const postRef = posts.doc(req.params.id);
+    const postRef = posts.doc(request.params.id);
     const post = await postRef.get();
     if (!post.exists) {
-      return res.status(404).send('Post not found');
+      return response.status(404).send('Post not found');
       }
   // Considering the request parameters to locate the post by ID
+    
       const postData = post.data();
       postData.comments.push(comment);
       await postRef.update({ comments: postData.comments });
   // Saving the updated post with new comments.
- res.send('Comment added');
+    
+ response.send('Comment added');
     } catch (err) {
-      res.status(400).send(err.message);
+      response.status(400).send(err.message);
     }
   });
 
