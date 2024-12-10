22d39121fa7038f8762bdafd0a2a6892f7d00d7e
halbel01 <125689963+halbel01@users.noreply.github.com>
2024-12-10 15:40:57 +0000

    Update schemaPost.js

--- schemas/schemaPost.js
+++ schemas/schemaPost.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const postSchema = new mongoose.Schema({
   title: { type: String, required: true },
   category: { type: String, required: true }, 
-  main: { type: String, required: true },
+  mainBody: { type: String, required: true },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
 // Separating the post into different categories
 
